dcf341c80db22947765bad7a5e97f6184ce18a01
latesc284-creator <arijoose@gmail.com>
2026-05-07 16:38:23 -0300

    CORREJI EL IMPORT

--- src/Controllers/Players/addCredits.js
+++ src/Controllers/Players/addCredits.js
@@ -13,13 +13,11 @@ export const addCredits = async (req, res) => {
       return sendResponse(res, 400, "Invalid data");
     }
 
-
     const admin = await AdminModel.searchId(idAdmin);
 
     if (!admin) {
       return sendResponse(res, 400, "Admin not found");
     }
-
 
     const player = await PlayerModel.exsitPlayer(UserName);
     if (!player) {
@@ -27,16 +25,12 @@ export const addCredits = async (req, res) => {
     }
 
     // 🔥 incremento seguro
-    const updatedPlayer = await PlayerModel.updateCredits(
-      player._id,
-      amount
-    );
+    const updatedPlayer = await PlayerModel.updateCredits(player._id, amount);
 
     return sendResponse(res, 200, "Credits added", {
-    UserName: updatedPlayer.UserName,
+      UserName: updatedPlayer.UserName,
       credits: updatedPlayer.credits,
     });
-
   } catch (error) {
     console.error("addCredits error:", error);
     return sendResponse(res, 500, "Error adding credits");
